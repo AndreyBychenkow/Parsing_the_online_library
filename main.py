f033db71ec2c4e299f31210ba961f417fd28be57
AndreyBychenkow <decebell032@yandex.ru>
2024-12-06 22:18:23 +0300

    Download books with titles

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 
 import requests
+from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename
 from requests.exceptions import HTTPError
 
 
@@ -9,39 +11,58 @@ def check_for_redirect(response):
         raise HTTPError("Редирект на главную страницу.")
 
 
-def download_book(book_id, folder='books'):
-    url = f"https://tululu.org/txt.php?id={book_id}"
+def fetch_page(url):
     response = requests.get(url)
-
-    if not response.ok:
-        raise HTTPError(f"Не удалось получить данные для книги с ID {book_id}.")
-
+    response.raise_for_status()
     check_for_redirect(response)
+    return response
 
-    filename = os.path.join(folder, f'id_book_{book_id}.txt')
-    with open(filename, 'wb') as file:
+
+def download_txt(book_id, folder='books'):
+    url = f"https://tululu.org/txt.php?id={book_id}"
+    response = fetch_page(url)
+
+    os.makedirs(folder, exist_ok=True)
+
+    title = _parse_book_page(book_id)
+
+    safe_filename = f"{book_id}. {sanitize_filename(title)}.txt"
+    filepath = os.path.join(folder, safe_filename)
+
+    with open(filepath, 'wb') as file:
         file.write(response.content)
 
-    return filename
+    return filepath
 
 
-def read_book_content(filepath):
-    with open(filepath, 'r', encoding='utf-8', errors='ignore') as file:
-        return file.read()
+def _parse_book_page(book_id):
+    url = f"https://tululu.org/b{book_id}/"
+    response = fetch_page(url)
+
+    soup = BeautifulSoup(response.text, 'lxml')
+    title_tag = soup.find('div', id='content').find('h1')
+
+    if title_tag:
+        title_parts = title_tag.text.split(':')
+        return title_parts[0].strip()
+
+    return f"Книга_{book_id}"
 
 
 def main():
-    os.makedirs('books', exist_ok=True)
+    downloaded_books = []
 
     for book_id in range(1, 11):
         try:
-            filepath = download_book(book_id)
-            content = read_book_content(filepath)
-            print(f"Книга с ID {book_id} успешно скачана.")
+            filepath = download_txt(book_id)
+
+            if filepath:
+                title = _parse_book_page(book_id)
+                downloaded_books.append((book_id, title))
+                print(f"{book_id}. {title}.txt")
+
         except requests.HTTPError as e:
-            print(f"Ошибка при скачивании книги с ID {book_id}: {e}")
-        except requests.DecodeError as e:
-            print(f"Произошла ошибка при обработке книги с ID {book_id}: {e}")
+            print(f"{book_id}. Ошибка при скачивании книги: {e}")
 
     print("Все доступные книги обработаны.")
 
