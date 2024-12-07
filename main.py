b996500d71062240f35d4676f32f47ec25d62bcc
AndreyBychenkow <decebell032@yandex.ru>
2024-12-07 15:59:59 +0300

    Download book covers

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from urllib.parse import urljoin, urlsplit, unquote
 
 import requests
 from bs4 import BeautifulSoup
@@ -35,6 +36,33 @@ def download_txt(book_id, folder='books'):
     return filepath
 
 
+def download_image(url, folder='images'):
+    response = fetch_page(url)
+
+    os.makedirs(folder, exist_ok=True)
+
+    filename = unquote(urlsplit(url).path.split('/')[-1])
+    filepath = os.path.join(folder, filename)
+
+    with open(filepath, 'wb') as file:
+        file.write(response.content)
+
+    return filepath
+
+
+def get_book_cover_url(book_id):
+    url = f"https://tululu.org/b{book_id}/"
+    response = fetch_page(url)
+
+    soup = BeautifulSoup(response.text, 'lxml')
+    img_tag = soup.find('div', class_='bookimage').find('img')
+
+    if img_tag and img_tag.get('src'):
+        return urljoin(url, img_tag['src'])
+
+    return None
+
+
 def _parse_book_page(book_id):
     url = f"https://tululu.org/b{book_id}/"
     response = fetch_page(url)
@@ -50,19 +78,21 @@ def _parse_book_page(book_id):
 
 
 def main():
-    downloaded_books = []
-
-    for book_id in range(1, 11):
+    for book_id in range(5, 11):
         try:
-            filepath = download_txt(book_id)
+            download_txt(book_id)
 
-            if filepath:
-                title = _parse_book_page(book_id)
-                downloaded_books.append((book_id, title))
-                print(f"{book_id}. {title}.txt")
+            title = _parse_book_page(book_id)
+            cover_url = get_book_cover_url(book_id)
+
+            if title and cover_url:
+                filepath = download_image(cover_url)
+                print(f"\nЗаголовок: {title}\nСсылка на картинку: {cover_url}\nОбложка: {filepath}\n")
 
         except requests.HTTPError as e:
-            print(f"{book_id}. Ошибка при скачивании книги: {e}")
+            print(f"Ошибка: Книга с id {book_id} не доступна. {e}\n")
+        except requests.OSError:
+            print(f"Книга с id {book_id}. Ошибка записи файла\n")
 
     print("Все доступные книги обработаны.")
 
